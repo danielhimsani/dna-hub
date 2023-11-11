d4f124d5ebfa694ed6ea441bca33f9ae3c28169e
daniel himsani <danielhimsani@gmail.com>
2023-11-11 23:59:22 +0200

    A

--- main_server.py
+++ main_server.py
@@ -1,7 +1,8 @@
 import json
+from functools import wraps
 
 from bson import json_util
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_cors import CORS
 
 from mongo_dal import MongoDal, KisserNotExist, KisserAlreadyExist
@@ -10,20 +11,51 @@ app = Flask(__name__, static_folder="dist/")
 CORS(app)
 mongo_connection = None
 
+
+def add_cors_preflight_headers(response):
+    allow_request = 'api' in request.origin
+    if allow_request:
+        response.headers['Access-Control-Allow-Origin'] = request.origin
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        # Allow chrome to access private network ajax requests
+        response.headers['Access-Control-Allow-Private-Network'] = 'true'
+    return response
+
+def handle_cors(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        if request.method == 'OPTIONS':
+            response = Response()
+        else:
+            response = func(*args, **kwargs)
+        response = add_cors_preflight_headers(response)
+        return response
+    return decorator
+
+
+
 @app.errorhandler(404)
+@handle_cors
 def not_found(e):
     return app.send_static_file('index.html')
 
+
 @app.route('/')
+@handle_cors
 def index():
     return app.send_static_file("index.html")
 
+
 @app.route('/<path:path>', methods=['GET'])
+@handle_cors
 def static_proxy(path):
     return app.send_static_file(path)
 
 
 @app.route('/api/new_kisser', methods=['POST'])
+@handle_cors
 def add_kisser():
     try:
         request_body = request.get_json()
@@ -43,6 +75,7 @@ def add_kisser():
 
 
 @app.route('/api/new_kiss', methods=['POST'])
+@handle_cors
 def add_kiss():
     try:
         request_body = request.get_json()
@@ -57,16 +90,19 @@ def add_kiss():
 
 
 @app.route('/api/get_kisser', methods=['GET'])
+@handle_cors
 def get_kisser():
     return str(list(mongo_connection.kissers_collection.find(request.get_json())))
 
 
 @app.route('/api/get_kiss', methods=['GET'])
+@handle_cors
 def get_kiss():
     return str(list(mongo_connection.kisses_collection.find(request.get_json())))
 
 
 @app.route('/api/get_all_kissers', methods=["GET"])
+@handle_cors
 def get_all_kissers():
     kissers = list(mongo_connection.kissers_collection.find(request.get_json()))
     return {
@@ -75,6 +111,7 @@ def get_all_kissers():
 
 
 @app.route('/api/get_all_kisses', methods=['GET'])
+@handle_cors
 def get_all_kisses():
     kissers = list(mongo_connection.kissers_collection.find(request.get_json()))
     kisses = list(mongo_connection.kisses_collection.find(request.get_json()))
